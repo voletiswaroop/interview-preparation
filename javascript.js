92b89550958d1196bec2c1d4c788b26f0b97c12b
Swaroop Gupta Voleti <voleti.swaroop@gmail.com>
2022-05-18 22:03:14 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+Array Methods in Javascript?
+- push() :- We use this to add an element at the end of an array.
+- pop() :-  We use this to remove the last element of an array.
+- shift() :- We use this to remove the first element of an array.
+- unshift() :- We use this to add an element at the begining of an array.
+- join() :- We use this to concatenate or join all the elements in the array into one string. We can use our preferred connectors inside the ' ' 
+            single quotes.
+- sort() :- We use this to sort the elements of an array
+- indexOf() :- We use this to find the index of an element in an array
+- reverse() :- We use this to reverse the elements in an array
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+              
